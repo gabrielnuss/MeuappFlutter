2f361e1530c7743aa131b9e2500ee591a854415f
Prog 22 <prog22@sci10.local>
2023-09-27 13:27:51 -0300

    Alterações desafio DIO

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,10 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple.shade700),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Página principal'),
     );
   }
 }
@@ -55,16 +55,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _contador = 0;
 
-  void _incrementCounter() {
+  void _incrementarContador() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _contador++;
     });
   }
 
@@ -105,20 +105,19 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+            const Text('Você pressionou o botão essa quantidade:',
+                style: TextStyle(color: Colors.green, fontSize: 40)),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '$_contador',
+              style: TextStyle(color: Colors.amber, fontSize: 40),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: _incrementarContador,
+        tooltip: 'Adicionar',
+        child: const Icon(Icons.add_box),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
